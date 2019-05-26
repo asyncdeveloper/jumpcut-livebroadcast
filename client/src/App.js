ec23a3a551856732b8758a6979275619a2ee8e54
Sheyilaaw <sheyilaaw98@gmail.com>
2019-05-26 22:00:49 +0100

    Add appropriate route url for signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="App">
-        <SignUp />
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Switch>
+                  <Route path='/signup' component={SignUp} />
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
