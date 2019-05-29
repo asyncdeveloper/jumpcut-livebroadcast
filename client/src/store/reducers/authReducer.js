f501eb24c287e15813dfd2d8ce609bd2efb088f0
Sheyilaaw <sheyilaaw98@gmail.com>
2019-05-29 22:58:38 +0100

    Refactor authentication

--- client/src/store/reducers/authReducer.js
+++ client/src/store/reducers/authReducer.js
@@ -1,24 +1,26 @@
+import { SIGNIN_FAILURE, SIGNIN_SUCCESS, SIGNUP_FAILURE, SIGNUP_SUCCESS } from "../actions/authActions";
+
 const initialState = {
     authError: null
 };
 const authReducer = (state = initialState , action) => {
     switch(action.type) {
-        case 'SIGNUP_SUCCESS':
+        case SIGNUP_SUCCESS:
             return {
                 ...state,
                 authError: null
             };
-        case 'SIGNUP_FAILURE':
+        case SIGNUP_FAILURE:
             return {
                 ...state,
                 authError: action.error.message
             };
-        case 'SIGNIN_SUCCESS':
+        case SIGNIN_SUCCESS:
             return {
                 ...state,
                 authError: null
             };
-        case 'SIGNIN_FAILURE':
+        case SIGNIN_FAILURE:
             return {
                 ...state,
                 authError: action.error.message
