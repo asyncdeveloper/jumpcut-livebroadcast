c8a149c27afde8f27ec64ad991f3ce2658e19015
Oluwaseyi Adeogun <Sheyilaaw98@gmail.com>
2019-06-03 08:15:36 +0100

    Revert "Add appropriate route url for signup page"
    This reverts commit b736f194012e7f595c0a6b95bbc7dea88410ebdc.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SignUp from "./components/SignUp";
 
 function App() {
   return (
-      <BrowserRouter>
-          <div className="App">
-              <Switch>
-                  <Route path='/signup' component={SignUp} />
-              </Switch>
-          </div>
-      </BrowserRouter>
+    <div className="App">
+        <SignUp />
+    </div>
   );
 }
 
